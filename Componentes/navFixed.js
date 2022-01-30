983c5432f773742b99da15c21228f865d9d84460
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-01-30 18:07:42 -0300

    Retirando animação de menu

--- Componentes/navFixed.js
+++ Componentes/navFixed.js
@@ -1,14 +1,20 @@
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos >= currentScrollPos) {
-    document.getElementById("navbar").style.position = ` fixed `
-    document.getElementById("navbar").style.top = "0";
 
-  } else {
-    document.getElementById("navbar").style.position = ` fixed `
-    document.getElementById("navbar").style.top = "-8.5rem";
+window.onscroll = function() {
+  const larguraBody = screen.width
+  var currentScrollPos = window.pageYOffset;
+  if(larguraBody >= 1024){
+
+    if (prevScrollpos >= currentScrollPos) {
+      document.getElementById("navbar").style.position = ` fixed `
+      document.getElementById("navbar").style.top = "0";
+  
+    } else {
+      document.getElementById("navbar").style.position = ` fixed `
+      document.getElementById("navbar").style.top = "-8.5rem";
+    }
+    prevScrollpos = currentScrollPos;
+  
   }
-  prevScrollpos = currentScrollPos;
 
 }
