00612eb261d2ef222503b34f6cd8ac2d495df226
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-02-14 19:34:52 -0300

    Ajustando texto de 2017 para 2018 e adicionando case de IndustryCare

---
+++ Componentes/glide.js
@@ -0,0 +1,7 @@
+import Glide from '@glidejs/glide'
+
+new Glide('.glide').detach()
+
+import Glide, { Anchors } from './@glidejs/glide/dist/glide.modular.esm'
+
+new Glide('.glide').detach({ Anchors })
