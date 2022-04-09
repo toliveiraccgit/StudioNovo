71f9411ceb9d17aec1a10f45e9fa6bc128c0eaa7
Diogo-Ol <oliveiradiogo50@gmail.com>
2022-04-09 12:04:56 -0300

    Testando email

---
+++ Componentes/FormContato/enviaEmail.php
@@ -0,0 +1,35 @@
+<?php
+//1 – Definimos Para quem vai ser enviado o email
+$para = "olvieiradiogo50@gmail.com";
+//2 - resgatar o nome digitado no formulário e  grava na variavel $nome
+$nome = $_POST['nome'];
+// 3 - resgatar o assunto digitado no formulário e  grava na variavel
+//$assunto
+
+$empresa = $_POST['empresa'];
+$email = $_POST['email'];
+$mensagem = $_POST['mensagem'];
+ //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+$mensagem = "<strong>Nome:  </strong>".$nome;
+$mensagem = "<strong>Nome Empresa:  </strong>".$empresa;
+$mensagem = "<strong>E-Mail:  </strong>".$email;
+$mensagem .= "<br>  <strong>Mensagem: </strong>"
+.$_POST['mensagem'];
+
+//5 – agora inserimos as codificações corretas e  tudo mais.
+$headers =  "Content-Type:text/html; charset=UTF-8\n";
+$headers .= "From:  https://toliveiraccgit.github.io/StudioNovo/<$email>\n";
+//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+$headers .= "X-Sender:  <$email>\n";
+//email do servidor //que enviou
+$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+$headers .= "Return-Path:  <$email>\n";
+//caso a msg //seja respondida vai para  este email.
+$headers .= "MIME-Version: 1.0\n";
+
+mail($para, $assunto, $mensagem, $headers);  //função que faz o envio do email.
+?>
+
+
+
---
+++ Componentes/FormContato/mudaLabel.js
@@ -0,0 +1,51 @@
+const labelInput1 = document.querySelector('.label__input___form-contato')
+
+function mudaCorBranco1(){
+    labelInput1.style.color = ` #FDFEFF `
+
+}
+
+function mudaCorCinza1(){
+    labelInput1.style.color = ` #666666 `
+}
+
+
+
+const labelInput2 = document.querySelector('#label-form-2')
+
+function mudaCorBranco2(){
+    labelInput2.style.color = ` #FDFEFF `
+
+}
+
+function mudaCorCinza2(){
+    labelInput2.style.color = ` #666666 `
+}
+
+
+
+const labelInput3 = document.querySelector('#label-form-3')
+
+function mudaCorBranco3(){
+    labelInput3.style.color = ` #FDFEFF `
+
+}
+
+function mudaCorCinza3(){
+    labelInput3.style.color = ` #666666 `
+}
+
+
+
+
+const labelInput4 = document.querySelector('#label-form-4')
+
+
+function mudaCorBranco4(){
+    labelInput4.style.color = ` #FDFEFF `
+
+}
+
+function mudaCorCinza4(){
+    labelInput4.style.color = ` #666666 `
+}
